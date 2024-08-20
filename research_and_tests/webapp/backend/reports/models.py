353a53a65abd2e6b9ffd75cedb65fda0c8e6b007
DanielMilstein <dnmilstein@miuandes.cl>
2024-08-20 19:08:12 -0400

    datos

---
+++ research_and_tests/webapp/backend/reports/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from clips.models import Clip, Author
+
+# Create your models here.
+
+class Report(models.Model):
+    report_id = models.CharField(max_length=255, primary_key=True)
+    audio_clips = models.ManyToManyField('clips.Clip')
+    author = models.ForeignKey('clips.Author', on_delete=models.CASCADE)
+    summary = models.TextField()
+    report_objective = models.ForeignKey('ReportObjective', on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __init__(self, report_id, author, report_objective):
+        self.report_id = report_id
+        self.audio_clips = []
+        self.author = author
+        self.summary = ''
+        self.report_objective = report_objective
+        self.date = report_id.split('_')[1]
+
+
+class ReportObjective(models.Model):
+    sentence = models.TextField()
