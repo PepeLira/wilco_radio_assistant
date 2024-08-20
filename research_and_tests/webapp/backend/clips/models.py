3afff5c136476b6015da8d863a4fb1431fda78f5
DanielMilstein <dnmilstein@miuandes.cl>
2024-08-20 12:42:35 -0400

    django backend

---
+++ research_and_tests/webapp/backend/clips/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    bio = models.TextField()
+
+    def __init__(self, name, email, bio):
+        self.name = name
+        self.email = email
+        self.bio = bio
+
+    def __str__(self):
+        return f'Author {self.name}'
+
+    def __repr__(self):
+        return f'Author {self.name}'
+
+class Clip(models.Model):
+    clip_id = models.CharField(max_length=255, primary_key=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    audio = models.FileField()
+    time_start = models.DateTimeField()
+    date = models.DateField()
+    transcript = models.TextField()
+    entities = models.TextField()
+    keywords = models.TextField()
+    summary = models.TextField()
+
+    def __init__(self, clip_id, author, audio, time_start):
+        self.clip_id = clip_id
+        self.author = author
+        self.audio = audio
+        self.time_start = time_start
+        self.date = clip_id.split('_')[1]
+        self.transcript = ''
+        self.entities = []
+        self.keywords = []
+        self.summary = ''
+
+    def transcribe_audio(self):
+        pass
+
+    def extract_entities(self):
+        pass
+
+    def extract_keywords(self):
+        pass
+
+    def summarize(self):
+        pass
+
+    def __str__(self):
+        return f'Clip {self.clip_id} by {self.author}'
+
+    def __repr__(self):
+        return f'Clip {self.clip_id} by {self.author}'
